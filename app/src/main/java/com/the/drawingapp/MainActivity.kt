e9cfad0fab8bfc31722458d854d29f310a2716d5
Nam Nguyen <nhhn1201@gmail.com>
2024-02-10 14:58:23 -0700

    Initialized ViewModel

--- app/src/main/java/com/the/drawingapp/MainActivity.kt
+++ app/src/main/java/com/the/drawingapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.the.drawingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel : DrawingViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_screen_layout)

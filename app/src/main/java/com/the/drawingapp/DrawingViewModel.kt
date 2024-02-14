38f6af99b460d14273ea2025c49b79d7addbcc76
Nam Nguyen <nhhn1201@gmail.com>
2024-02-14 15:43:54 -0700

    Since Color and StrokeWidth is now handled in DrawableFragment, remove their LiveData, only observe canvasBitmap changes now.

--- app/src/main/java/com/the/drawingapp/DrawingViewModel.kt
+++ app/src/main/java/com/the/drawingapp/DrawingViewModel.kt
@@ -6,30 +6,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DrawingViewModel : ViewModel() {
-    private val _currentColor = MutableLiveData<Int>()
-    val currentColor: LiveData<Int>
-        get() = _currentColor
-
-    private val _currentStrokeWidth = MutableLiveData<Float>()
-    val currentStrokeWidth: LiveData<Float>
-        get() = _currentStrokeWidth
-
     private val _canvasBitmap = MutableLiveData<Bitmap>()
-    val canvasBitmap: LiveData<Bitmap>
-        get() = _canvasBitmap
+    val canvasBitmap: LiveData<Bitmap> = _canvasBitmap
 
-    fun setColor(color: Int){
-        _currentColor.value = color
-    }
-
-    fun setStrokeWidth(strokeWidth: Float){
-        _currentStrokeWidth.value = strokeWidth
-    }
-
-    fun updateBitmap(bitmap: Bitmap){
-        _canvasBitmap.value = bitmap
-    }
-    fun setDrawingBitmap(bitmap: Bitmap){
+    fun updateBitmap(bitmap: Bitmap) {
         _canvasBitmap.value = bitmap
     }
 }

832461df880911f385047e5ac93745cb32260f80
Nam Nguyen <nhhn1201@gmail.com>
2024-02-14 22:53:45 -0700

    DrawingView is now only handling invalidation

--- app/src/main/java/com/the/drawingapp/DrawingView.kt
+++ app/src/main/java/com/the/drawingapp/DrawingView.kt
@@ -5,24 +5,19 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    private var canvasBitmap: Bitmap = Bitmap.createBitmap(800, 800, Bitmap.Config.ARGB_8888)
-    val canvas = Canvas(canvasBitmap)
-
-    init {
-        canvasBitmap.eraseColor(Color.WHITE)
-    }
+    private var canvasBitmap: Bitmap? = null
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawBitmap(canvasBitmap, 0f, 0f, null)
+        canvasBitmap?.let { canvas.drawBitmap(it, 0f, 0f, null) }
     }
 
     fun setBitmap(bitmap: Bitmap){
         canvasBitmap = bitmap
+        invalidate()
     }
-
-    fun getBitmap(): Bitmap = canvasBitmap
 
 }

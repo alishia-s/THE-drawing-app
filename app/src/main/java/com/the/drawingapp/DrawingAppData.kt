7c22d0985a191a41e877509c95d07fb85b2d7133
Nam Nguyen <nhhn1201@gmail.com>
2024-04-15 18:07:37 -0600

    Added additional columns needed to keep track of sharing

--- app/src/main/java/com/the/drawingapp/DrawingAppData.kt
+++ app/src/main/java/com/the/drawingapp/DrawingAppData.kt
@@ -15,17 +15,32 @@ class Converters {
     fun dateToTimestamp(date: Date?): Long? {
         return date?.time?.toLong()
     }
+
+    @TypeConverter
+    fun fromCsv(value: String?): List<String>? {
+        return value?.split(",") ?: emptyList()
+    }
+
+    fun toCsv(list: List<String>?) : String {
+        return list?.joinToString { "," } ?: ""
+    }
 }
 
 //when save is pressed, save and send it as a DrawingAppData
 @Entity(tableName = "drawings")
-data class DrawingAppData(var timestamp: Date,
-                          var name : String,
-                          var imageUrl : String? = null,
-                          var isSynced : Boolean = false) {
+data class DrawingAppData(
+    var userId: String,
+    var name: String,
+    var timestamp: Date,
+    var imageUrl: String? = null,
+    var isSynced: Boolean = false,
+    var sharedWith: String = ""
+) {
     //will autoincrement
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 
-    fun replaceName(newName : String) {name = newName}
+    fun replaceName(newName: String) {
+        name = newName
+    }
 }
